b5ddbe92c20d35acc5e12923f7267061450da2c6
cifjovo <cifjovo@gmail.com>
2022-10-05 23:08:17 +0200

    05.10.2022

---
+++ js/index.js
@@ -0,0 +1,23 @@
+// console.log("Hello");
+
+// let y=100;
+// let x=120;
+// if(y>x){
+//     console.log('The first number is greater than the second number');
+// }
+// else if (x>y){
+//     console.log('x is greater');
+// }
+
+{
+
+
+case false: console.log("number two is greater");
+break;
+default: console.log("They are the same")
+}
+
+const arrayOfNumbers = []
+//array literal;
+const listOfStudents = ["Damilola", 1000,"Hamdia", {},]
+const listOfStudentsscore = [9, 10, 13,]
